534a7797257b85ce3cd03d8e3aca47a9639c5fac
Umer <umer.farooq.042@gmail.com>
2019-02-24 01:59:21 +0500

    I don't know what I'm doing

--- client.py
+++ client.py
@@ -3,17 +3,14 @@ import argparse
 
 def main():
 	parser = argparse.ArgumentParser(description='Starts the client. ')
-	parser.add_argument('ip_adress', nargs=1, default='192.168.10.1')
+	parser.add_argument('ip_adress', nargs=1, default='192.168.10.4')
 	parser.add_argument('port', type=int, nargs=1, default=9999)
-
 	args = parser.parse_args()
-	# print(args.ip_adress)
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	host = socket.gethostname()
+	host = args.ip_adress[0]
 	port = args.port[0]
 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, port))
 	print('Connected to ', host, port)
 
@@ -24,6 +21,7 @@ def main():
 		s.send(msg.encode('utf-8'))
 		msg = s.recv(1024)
 		print(msg.decode('utf-8'))
+
 	s.close()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket               # Import socket module
+import socket
 import threading
 import argparse
 
@@ -15,30 +15,31 @@ def on_new_client(clientsocket,addr):
 
 def main():
 	parser = argparse.ArgumentParser(description='Starts the server. ')
-	parser.add_argument('ip_adress', nargs=1, default='192.168.10.1')
+	parser.add_argument('ip_adress', nargs=1, default='192.168.10.4')
 	parser.add_argument('port', type=int, nargs=1, default=9999)
 	parser.add_argument('players', type=int, nargs=1, default=2)
-
 	args = parser.parse_args()
-	# print(args.ip_adress)
-	# print(args.port)
-	# print(args.players)
+
+	host = args.ip_adress[0]
+	port = args.port[0]                 # Reserve a port for your service.
+	max_players = args.players[0]
+	current_players = 0
+	players = []
 
 	s = socket.socket()         # Create a socket object
-	host = socket.gethostname() # Get local machine name
-	port = args.port[0]                 # Reserve a port for your service.
-
-	print ('Server started!')
-	print ('Waiting for clients...')
-
 	s.bind((host, port))        # Bind to the port
-	s.listen(1)                 # Now wait for client connection.
+	s.listen()                 # Now wait for client connection.
+	print('Server started!')
+	print('Waiting for clients...')
 
-	while True:
+	while current_players < max_players:						# Wait for players to join
 		c, addr = s.accept()     # Establish connection with client.
+		current_players = current_players + 1
 		t = threading.Thread(target=on_new_client, args=(c,addr))
 		t.daemon = True
 		t.start()
+		players.append(t)
+
 	s.close()
 
 if __name__ == '__main__':
